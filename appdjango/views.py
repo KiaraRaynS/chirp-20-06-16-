1db94e4ccc92e1787273c1c7de84934e023e985b
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-21 13:06:52 -0400

    Timecheck enabled, fixed logout

--- appdjango/views.py
+++ appdjango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView
 from appdjango.models import Chirp
 
 
@@ -11,3 +11,21 @@ class ViewIndex(ListView):
         context = super().get_context_data(**kwargs)
         context['amount'] = Chirp.objects.all().count
         return context
+
+
+class ChirpDetailView(DetailView):
+    model = Chirp
+
+    def get_queryset(self):
+        return Chirp.objects.filter(user=self.request.user)
+
+
+class CreateChirp(CreateView):
+    model = Chirp
+    fields = ['body']
+    success_url = '/'
+
+    def form_valid(self, form):
+        chirp = form.save(commit=False)
+        chirp.user = self.request.user
+        return super().form_valid(form)
--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from appdjango.views import ViewIndex
+from appdjango.views import ViewIndex, ChirpDetailView, CreateChirp
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', ViewIndex.as_view(), name='index'),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^chirpdata/(?P<pk>\d+)/$', ChirpDetailView.as_view(), name='detailview'),
+    url(r'^createchirp/$', CreateChirp.as_view(), name='chirpcreate')
 ]
